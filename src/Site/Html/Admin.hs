d56de90773f56f7a6ba65fcf1487a8123d6389bc
Erik Aker <eraker@gmail.com>
2018-09-06 20:41:39 -0700

    Add content-encoding to resource types so we can use either html or md

--- src/Site/Html/Admin.hs
+++ src/Site/Html/Admin.hs
@@ -15,7 +15,7 @@ import           Site.Types
 adminEditListPage :: PageNum -> [Resource] -> Html
 adminEditListPage pgNum content = Base.pageSkeleton $ contentEditList pgNum content
 
-adminEditDetailPage :: Resource -> Html
+adminEditDetailPage :: Maybe Resource -> Html
 adminEditDetailPage item = Base.pageSkeleton $ contentEditDetail item
 
 
@@ -38,6 +38,7 @@ renderTableHead =
       H.tr $ do
         H.td "Id"
         H.td "Type"
+        H.td "Content"
         H.td "Published"
         H.td "Publish Date"
         H.td "Title"
@@ -48,8 +49,9 @@ renderTableHead =
 renderTableRow :: Resource -> Html
 renderTableRow item = 
   H.tr $ do
-    H.td $ H.toMarkup (item ^. pid)
+    H.td $ H.a ! A.href (H.toValue (item ^. pid)) $ H.toMarkup (item ^. pid)
     H.td $ H.toMarkup $ show (item ^. resourceType)
+    H.td $ H.toMarkup $ show (item ^. contentEncoding)
     H.td $ renderPublishedCheck (item ^. published)
     H.td $ H.toMarkup (item ^. pubdate)
     H.td $ H.toMarkup (item ^. title)
@@ -70,7 +72,54 @@ renderFeaturedImg (Just img) = H.img ! A.src (H.toValue img)
 
 
 -- | Content Edit Detail: Edit a particular item
-contentEditDetail :: Resource -> Html
-contentEditDetail = undefined
+contentEditDetail :: Maybe Resource -> Html
+contentEditDetail Nothing = newContentItemForm
+contentEditDetail (Just item) = updateContentItem item
 
 -- | Edit Detail helpers
+newContentItemForm :: Html
+newContentItemForm = undefined
+
+
+updateContentItem :: Resource -> Html
+updateContentItem item = do
+  H.div ! A.class_ "edit-head" $
+    H.h1 $ (H.toMarkup $ item ^. pid) <> (H.toMarkup . show $ item ^. resourceType)
+
+  H.form ! A.method "post" ! A.action "/admin/item" $
+    H.div ! A.class_ "row" $ do
+      H.div ! A.class_ "two columns" $ do
+        H.label ! A.for "pid" $ "Post ID"
+        H.input ! A.type_ "text" ! A.id "pid" ! A.name "_pid" ! A.readonly "true" ! A.value (H.toValue $ item ^. pid)
+      
+      H.div ! A.class_ "two columns" $
+        H.label ! A.for "published" $ do
+          "Post ID"
+          H.input ! A.type_ "checkbox" ! A.id "published" ! A.name "_published" ! A.value (H.toValue $ item ^. published)
+          H.span ! A.class_ "label-body" $ "Publish?"
+
+      H.div ! A.class_ "four columns" $ do
+        H.label ! A.for "resourceType" $ "Resource Type"
+        H.select ! A.class_ "u-fill-width" ! A.class_ "resourceType" $ do
+          H.select ! A.class_ "u-full-width" ! A.id "resourceType" ! A.name "_resourceType" ! A.value (H.toValue . show $ item ^. resourceType) $ do
+            H.option ! A.value "BlogPost" $ "BlogPost"
+            H.option ! A.value "BlogPost" $ "Project"
+            H.option ! A.value "BlogPost" $ "About"
+
+      H.div ! A.class_ "four columns" $ do
+        H.label ! A.for "pubdate" $ "Publication Date"
+        H.input ! A.type_ "text" ! A.id "pubdate" ! A.name "_pubdate" ! A.value (H.toValue $ item ^. pubdate)
+      
+
+
+  -- H.section ! A.id "contact" ! A.class_ "container contact-us u-full-width u-max-full-width" $
+
+  --   H.div ! A.class_ "eight columns edit-content-form" $
+  --     H.form ! A.method "post" ! A.action "/admin/item" $ do
+  --       -- _featuredImage
+  --       -- _published
+  --       H.input ! A.class_ "u-full-width" ! A.type_ "text" ! A.name "_title" ! A.id "titleInput" $ H.toMarkup $ item ^. title
+  --       H.input ! A.class_ "u-full-width" ! A.type_ "text" ! A.name "_pubdate" ! A.id "pudateInput" $ H.toMarkup $ item ^. pubdate
+  --       H.input ! A.class_ "u-full-width" ! A.type_ "text" !  A.name "_resourceType" ! A.placeholder "Email" ! A.id "resourceTypeInput"
+  --       H.textarea ! A.class_ "u-full-width" ! A.name "_body" ! A.id "body" $ H.toMarkup $ item ^. body
+  --       H.input ! A.class_ "button u-pull-right" ! A.type_ "submit" !  A.value "Send"
--- src/Site/Types.hs
+++ src/Site/Types.hs
@@ -4,10 +4,19 @@ module Site.Types where
 
 import           Control.Lens
 import           Data.Aeson
+import qualified Data.Text                         as T
 import           Data.Time.LocalTime
+import           GHC.Exts                          (IsList (..))
 import           GHC.Generics
 import           RIO
-import qualified Text.Blaze.Html5            as H
+import qualified Text.Blaze.Html5                   as H
+import           Web.FormUrlEncoded                (Form(..)
+                                                  , FromForm(..)
+                                                  , ToForm(..))
+import           Web.Internal.HttpApiData          (FromHttpApiData(..)
+                                                  , toQueryParam)
+import           Web.Internal.FormUrlEncoded       (parseUnique)
+
 
 
 type PageTotal = Int
@@ -15,18 +24,11 @@ type CurrentPage = Int
 type PageNum = (PageTotal, CurrentPage)
 type AllSiteTags = [Text]
 
-
-data ResourceType = BlogPost
-                   | About
-                   | Project
-                   deriving (Eq, Show, Generic)
-
-instance FromJSON ResourceType
-instance ToJSON ResourceType
-
+-- | Generic Resource object used across whole site
 data Resource = Resource {
   _pubdate :: Text
   , _resourceType :: ResourceType
+  , _contentEncoding :: ContentEncoding
   , _featuredImage :: Maybe Text
   , _published :: Bool
   , _body :: Text
@@ -38,5 +40,65 @@ data Resource = Resource {
 
 instance FromJSON Resource
 instance ToJSON Resource
+instance FromForm Resource
+
+
+-- | Resource Types used on the site: to be stored generically in JSON store (Elasticsearch)
+
+data ResourceType = BlogPost
+                   | About
+                   | Project
+                   deriving (Eq, Show, Generic)
+
+parseResourceType :: T.Text -> Either T.Text ResourceType
+parseResourceType val
+  | val == "blogpost" = Right BlogPost
+  | val == "about"    = Right About
+  | val == "project"  = Right Project
+  | otherwise         = Left ("Not a ResourceType: " <> val)
+
+instance FromJSON ResourceType
+instance ToJSON ResourceType
+
+instance ToForm ResourceType where
+  toForm rt = fromList [ (T.pack "_resourceType", T.pack . show $ rt) ]
+
+instance FromHttpApiData ResourceType where
+  parseUrlPiece = parseResourceType
+
+instance FromForm ResourceType where
+  fromForm frm = 
+    let parsed = parseUnique "_resourceType" frm :: Either T.Text T.Text
+    in join (parseResourceType <$> parsed)
+
+-- | ContentEncoding for differentiating type of content
+
+data ContentEncoding = ContentMarkdown 
+                      | ContentHtml
+                      deriving (Eq, Show, Generic, Read)
+
+instance ToForm ContentEncoding where
+  toForm ce = fromList [ (T.pack "_contentEncoding", T.pack . show $ ce) ]
+
+parseContentEncoding :: T.Text -> Either T.Text ContentEncoding
+parseContentEncoding val
+  | val == "markdown" = Right ContentMarkdown
+  | val == "html"    = Right ContentHtml
+  | otherwise         = Left ("Not a ContentEncoding: " <> val)
+
+
+instance FromHttpApiData ContentEncoding where
+  parseUrlPiece = parseContentEncoding
+
+instance FromForm ContentEncoding where
+  fromForm frm = 
+    let parsed = parseUnique "_contentEncoding" frm :: Either T.Text T.Text
+    in join (parseContentEncoding <$> parsed)
+  
+
+instance FromJSON ContentEncoding
+instance ToJSON ContentEncoding
+    
+-- | Lenses
 
 makeLenses ''Resource
